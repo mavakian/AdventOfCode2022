249f18fb65ed32242f952bc3994aa65d19f7b123
Michael Avakian <mavakian@iprotech.com>
2022-12-08 01:28:22 -0700

    Add project files.

---
+++ DayOne/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> caloriePacks = new List<int>();
+using (StreamReader reader = new StreamReader("C:\\Users\\mavakian\\source\\repos\\AdventOfCode\\DayOne\\input.txt"))
+{
+    string? snackCalories = string.Empty;
+    int caloriePack = 0;
+    while(snackCalories is not null)
+    {
+        while ((snackCalories = reader.ReadLine()) != string.Empty && snackCalories is not null)
+        {
+            caloriePack += Int32.Parse(snackCalories);
+        }
+        caloriePacks.Add(caloriePack);
+        caloriePack = 0;
+    }
+}
+foreach (int pack in caloriePacks)
+{
+    Console.WriteLine($"Index: {caloriePacks.IndexOf(pack)}, Calories: {pack}");
+}
+Console.WriteLine($"The elf with the most calories is elf number [{caloriePacks.IndexOf(caloriePacks.Max())-1}] with [{caloriePacks.Max()}] calories.");
+Console.WriteLine("Did I do good Michael?");
